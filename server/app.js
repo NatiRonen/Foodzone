56627dba3152d7875f450719548c92f059331592
Nati Ronen <ronennt@gmail.com>
2022-08-18 21:02:41 +0300

    set path cookies

--- server/app.js
+++ server/app.js
@@ -68,7 +68,7 @@ app.use(
       maxAge: 1000 * 60 * 15 * 1000, // 15 minutes
       sameSite: "none",
       secure: true,
-      domain: process.env.CLIENT_URL,
+      domain: process.env.SERVER_URL,
       path: "/",
     },
     store: store,
