966bef80c8d8753b71740d6890d372a71d03ceca
Ariel-levi <38177520+Ariel-levi@users.noreply.github.com>
2022-08-31 15:07:15 +0300

    login admin

--- client/src/admin/layoutAdmin.js
+++ client/src/admin/layoutAdmin.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AuthAdminComp from "../comps/auth/authAdminComp";
 import LottieAnimation from "../comps/misc/lottieAnimation";
-
 import HeaderAdmin from "./headerAdmin";
+
+import LoginAdmin from "./loginAdmin";
 
 function LayoutAdmin(props) {
   const [authorized, setAuthorized] = useState(false);
+
   return (
     <div>
       <React.Fragment>
@@ -15,6 +17,7 @@ function LayoutAdmin(props) {
         {!authorized ? (
           <LottieAnimation />
         ) : (
+          // <LoginAdmin />
           <>
             <HeaderAdmin />
             <Outlet />
