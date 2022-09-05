1036cef110379bd9770809af9d179c8e5cf6da44
Ariel-levi <38177520+Ariel-levi@users.noreply.github.com>
2022-09-05 17:49:05 +0300

    add timer to order

---
+++ client/src/comps/misc/timer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+// css
+import "../css/timer.css";
+
+function Timer(props) {
+  let time = props.time;
+
+  const formatRemainingTime = (_time) => {
+    const minutes = Math.floor((_time % 3600) / 60);
+    const seconds = _time % 60;
+
+    return `${minutes}:${seconds}`;
+  };
+
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="text_timer">Delivery Arrived 😋</div>;
+    }
+
+    return (
+      <div className="timer">
+        <div className="text_timer">Remaining time</div>
+        <div className="value">{formatRemainingTime(remainingTime)}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="timer_wrapper my-4">
+      <CountdownCircleTimer isPlaying duration={time} colors={["#5ea4f3"]}>
+        {renderTime}
+      </CountdownCircleTimer>
+    </div>
+  );
+}
+
+export default Timer;
